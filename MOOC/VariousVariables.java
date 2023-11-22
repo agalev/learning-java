60b31865354b7310409b80a4e550bdb1dac5f722
Alexander Galev <alexandergalev@Alexanders-MacBook-Pro.local>
2023-11-21 17:40:30 -0800

    MOOC course progress: variables

---
+++ MOOC/VariousVariables.java
@@ -0,0 +1,19 @@
+public class VariousVariables {
+    public static void main(String[] args) {
+        int number = 3;
+        double decimal = 5.5;
+        String string = "None!";
+
+        System.out.println("Chicken:");
+        System.out.println(number);
+        System.out.println("Bacon (kg):");
+        System.out.println(decimal);
+        System.out.println("Tractor:");
+        System.out.println(string);
+        System.out.println();
+        System.out.println("And finally, a summary:");
+        System.out.println(number);
+        System.out.println(decimal);
+        System.out.println(string);
+    }
+}
