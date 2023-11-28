8d2d4b9c58354cd688a501d668eae5777db40358
Alexander Galev <alex.galev@yahoo.com>
2023-11-28 14:10:28 -0800

    Part2: Loop exercise

--- MOOC/Part2/Factorial.java
+++ MOOC/Part2/Factorial.java
@@ -1,5 +1,18 @@
 import java.util.Scanner;
+/*
+ * Additional info:
+ * Factorials are used especially in probability calculus when examining different possible orders of a set.
+ * 
+ * For example, a group of five people can form 5! different lines, and a pack of 52 cards can be in 52! different orders.
+ * 
+ * Factorial can also be used to calculate combinations;
+ * For example it is possible to deal 52! / (5! * (52-5)!) different hands from a 52 card pack,
+ * 
+ * and
+ * 
+ * you can form 40! / (7! * (40 - 7)!) different 7 number lottery lines from 40 numbers.
 
+ */
 public class Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
---
+++ MOOC/Part2/LoopExercise.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class LoopExercise {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        int sum = 0;
+        int numbers = 0;
+        int even = 0;
+        int odd = 0;
+        System.out.println("Give numbers, '-1' quits:");
+
+        while (true) {
+            int input = Integer.valueOf(reader.nextLine());
+            if (input == -1) {
+                break;
+            } else {
+                if (input % 2 == 0) {
+                    even++;
+                } else {
+                    odd++;
+                }
+                sum+=input;
+                numbers++;
+            }
+        }
+        System.out.println("Thxbai");
+        System.out.println("Sum: " + sum);
+        System.out.println("Numbers: " + numbers);
+        System.out.println("Average: " + (double) sum/numbers);
+        System.out.println("Even: " + even);
+        System.out.println("Odd: " + odd);
+
+    }
+}
