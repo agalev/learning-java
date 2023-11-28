bed73a15ee78c6dc7436cfdead2c323b13c3695e
Alexander Galev <alex.galev@yahoo.com>
2023-11-28 11:54:08 -0800

    MOOC course progress: calculations and conditionals

--- MOOC/Conditionals.java
+++ MOOC/Conditionals.java
@@ -3,16 +3,20 @@ import java.util.Scanner;
 public class Conditionals {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Give the first number:");
-        int first = Integer.valueOf(scanner.nextLine());
-        System.out.println("Give the second number:");
-        int second = Integer.valueOf(scanner.nextLine());
-        if (first > second) {
-            System.out.println(first + " is greater than " + second + ".");
-        } else if (first < second) {
-            System.out.println(first + " is smaller than " + second + ".");
-        } else {
-            System.out.println(first + " is equal to " + second + ".");
+        System.out.println("Value of the gift?");
+        int giftAmount = Integer.valueOf(scanner.nextLine());
+        if (giftAmount < 5000) {
+            System.out.println("No tax!");
+        } else if (giftAmount > 5000 && giftAmount < 25000) {
+            System.out.println("Tax: " + (100 + (giftAmount - 5000) * 0.08));
+        } else if (giftAmount > 25000 && giftAmount < 55000) {
+            System.out.println("Tax: " + (1700 + (giftAmount - 25000) * 0.1));
+        } else if (giftAmount > 55000 && giftAmount < 200000) {
+            System.out.println("Tax: " + (4700 + (giftAmount - 55000) * 0.12));
+        } else if (giftAmount > 200000 && giftAmount < 1000000) {
+            System.out.println("Tax: " + (22100 + (giftAmount - 200000) * 0.15));
+        } else if (giftAmount > 1000000) {
+            System.out.println("Tax: " + (142100 + (giftAmount - 1000000) * 0.17));
         }
         scanner.close();
     }
