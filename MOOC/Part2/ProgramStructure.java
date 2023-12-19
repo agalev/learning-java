26432466e52664cc45f16f9957eb040ad84ae4fd
Alexander Galev <alex.galev@yahoo.com>
2023-12-18 17:00:07 -0800

    Part2: Methods!

--- MOOC/Part2/ProgramStructure.java
+++ MOOC/Part2/ProgramStructure.java
@@ -6,7 +6,9 @@ public static void main(String[] args) {
     // int input_number = Integer.valueOf(scanner.nextLine());
     // printUntilNumber(input_number);
     // scanner.close();
-    System.out.println(numberUno());
+    // System.out.println(numberUno());
+    int answer = sum4(4, 3, 6, 1);
+    System.out.println("sum: " + answer);
 }
 public static void printText() {
     System.out.println("In a hole in the ground there lived a method");
@@ -20,5 +22,8 @@ public static void printUntilNumber(int number) {
 }
 public static int numberUno() {
     return 1;
+}
+public static int sum4(int number1, int number2, int number3, int number4) {
+    return number1 + number2 + number3 + number4;
 }
 }
