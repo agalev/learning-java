c346d86d18d8eb4bdf445acfc632db8c2a3b3daf
Alexander Galev <alex.galev@yahoo.com>
2023-12-24 14:46:44 -0800

    Part4: OOP

--- MOOC/Part3/Main.java
+++ MOOC/Part3/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Account Jamey = new Account("Jamey", 100.00);
-        Jamey.printDetails();
-        Jamey.withdraw(20);
-        Jamey.printDetails();
-        Jamey.deposit(40.5);
-        Jamey.printDetails();
+        // Account Jamey = new Account("Jamey", 100.00);
+        // Jamey.printDetails();
+        // Jamey.withdraw(20);
+        // Jamey.printDetails();
+        // Jamey.deposit(40.5);
+        // Jamey.printDetails();
+        Whistle ravenWhistle = new Whistle("Kvaak");
+        ravenWhistle.sound();
     }
 }
---
+++ MOOC/Part3/Whistle.java
@@ -0,0 +1,9 @@
+public class Whistle {
+    private String sound;
+    public Whistle(String setSound) {
+        this.sound = setSound;
+    }
+    public void sound() {
+        System.out.println(this.sound);
+    }
+}
