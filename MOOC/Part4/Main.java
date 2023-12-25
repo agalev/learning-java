50c140233abe2a86535ef8181e822588975afc9e
Alexander Galev <alex.galev@yahoo.com>
2023-12-25 13:22:38 -0800

    Part4: OOP; Christmas study session

--- MOOC/Part4/Main.java
+++ MOOC/Part4/Main.java
@@ -17,20 +17,42 @@ public class Main {
         // }
 
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Item> items = new ArrayList<>();
-        while(true) {
-            System.out.print("Name of object ~ ");
-            String input = scanner.nextLine();
-            if (input.isEmpty()) {
-                scanner.close();
+        // ArrayList<Item> items = new ArrayList<>();
+        // while(true) {
+        //     System.out.print("Name of object ~ ");
+        //     String input = scanner.nextLine();
+        //     if (input.isEmpty()) {
+        //         scanner.close();
+        //         break;
+        //     }
+        //     items.add(new Item(input));
+        // }
+
+        // for (Item item:items) {
+        //     System.out.println(item);
+        // }
+
+        ArrayList<TelevisionProgram> TVs = new ArrayList<>();
+
+         while(true) {
+            System.out.print("Name of show ~ ");
+            String name = scanner.nextLine();
+            if (name.isEmpty()) {
                 break;
             }
-            items.add(new Item(input));
+            System.out.print (" Duration ~ ");
+            int duration = Integer.valueOf(scanner.nextLine());
+            TVs.add(new TelevisionProgram(name, duration));
         }
 
-        for (Item item:items) {
-            System.out.println(item);
+        System.out.print("Program's maximum duration? ~ ");
+        int maxDuration = Integer.valueOf(scanner.nextLine());
+
+        for (TelevisionProgram program:TVs) {
+            if (program.getDuration() < maxDuration) {
+                System.out.println(program);
+            }
+            scanner.close();
         }
-        
     }
 }
