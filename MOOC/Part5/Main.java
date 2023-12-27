47decdd4f8c720e27d8257fc038209338d567aae
Alexander Galev <alex.galev@yahoo.com>
2023-12-26 18:09:10 -0800

    Part5: OOP+ Creating ClockHand, Clock, Timer

--- MOOC/Part5/Main.java
+++ MOOC/Part5/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        // Clock clock = new Clock();
+
+        // while (true) {
+        //     System.out.println(clock);
+        //     clock.advance();
+        //     try {
+        //         Thread.sleep(1000);
+        //     } catch (Exception e) {
+        //         Thread.currentThread().interrupt();
+        //     }
+        // }
+
+        Timer timer = new Timer();
+
+        while (true) {
+            System.out.println(timer);
+            timer.advance();
+            try {
+                Thread.sleep(10);
+            } catch(Exception e) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 }
