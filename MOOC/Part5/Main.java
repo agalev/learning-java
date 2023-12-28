b959fdfe7eb039d99f0fdcf599098eb9e2128f5d
Alexander Galev <alex.galev@yahoo.com>
2023-12-27 17:33:53 -0800

    Part5: OOP+

--- MOOC/Part5/Main.java
+++ MOOC/Part5/Main.java
@@ -12,16 +12,19 @@ public class Main {
         //     }
         // }
 
-        Timer timer = new Timer();
+        // Timer timer = new Timer();
 
-        while (true) {
-            System.out.println(timer);
-            timer.advance();
-            try {
-                Thread.sleep(10);
-            } catch(Exception e) {
-                // Thread.currentThread().interrupt();
-            }
-        }
+        // while (true) {
+        //     System.out.println(timer);
+        //     timer.advance();
+        //     try {
+        //         Thread.sleep(10);
+        //     } catch(Exception e) {
+        //         // Thread.currentThread().interrupt();
+        //     }
+        // }
+
+        Book harryBook = new Book("J.K. Rowling", "Harry Potter and the Sorcerer's Stone", 223);
+        System.out.println(harryBook);
     }
 }
