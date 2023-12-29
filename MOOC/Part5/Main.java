73302441e16d4b23087549fe7250dbc5405c0a92
Alexander Galev <alex.galev@yahoo.com>
2023-12-28 20:16:43 -0800

    Part5: OOP+

--- MOOC/Part5/Main.java
+++ MOOC/Part5/Main.java
@@ -45,20 +45,38 @@ public class Main {
         // System.out.println(tapeMeasure);
         // System.out.println(plaster);
         // System.out.println(tyre);
-        Counter counter = new Counter();
-        System.out.println(counter.value());
+        // Counter counter = new Counter();
+        // System.out.println(counter.value());
 
-        counter.increaseValue();
-        System.out.println(counter.value());
+        // counter.increaseValue();
+        // System.out.println(counter.value());
 
-        counter.increaseValue(3);
-        System.out.println(counter.value());
+        // counter.increaseValue(3);
+        // System.out.println(counter.value());
 
-        counter.decreaseValue(2);
-        System.out.println(counter.value());
+        // counter.decreaseValue(2);
+        // System.out.println(counter.value());
 
-        counter.decreaseValue();
-        System.out.println(counter.value());
+        // counter.decreaseValue();
+        // System.out.println(counter.value());
 
+        HealthStation childrensHospital = new HealthStation();
+
+        Person ethan = new Person("Ethan", 1, 110, 7);
+        Person peter = new Person("Peter", 33, 176, 85);
+
+        System.out.println("weighings performed: " + childrensHospital.weighings());
+
+        childrensHospital.weigh(ethan);
+        childrensHospital.weigh(peter);
+
+        System.out.println("weighings performed: " + childrensHospital.weighings());
+
+        childrensHospital.weigh(ethan);
+        childrensHospital.weigh(ethan);
+        childrensHospital.weigh(ethan);
+        childrensHospital.weigh(ethan);
+
+        System.out.println("weighings performed: " + childrensHospital.weighings());
     }
 }
