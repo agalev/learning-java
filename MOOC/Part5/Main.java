00b4d32fb15da407c51614fe8fdb006ecf90436c
Alexander Galev <alex.galev@yahoo.com>
2023-12-28 20:51:18 -0800

    Part5: OOP+

--- MOOC/Part5/Main.java
+++ MOOC/Part5/Main.java
@@ -79,16 +79,28 @@ public class Main {
 
         // System.out.println("weighings performed: " + childrensHospital.weighings());
 
-        PaymentCard petesCard = new PaymentCard(10);
+        // PaymentCard petesCard = new PaymentCard(10);
 
-        System.out.println("money " + petesCard.balance());
-        boolean wasSuccessful = petesCard.takeMoney(8);
-        System.out.println("successfully withdrew: " + wasSuccessful);
-        System.out.println("money " + petesCard.balance());
+        // System.out.println("money " + petesCard.balance());
+        // boolean wasSuccessful = petesCard.takeMoney(8);
+        // System.out.println("successfully withdrew: " + wasSuccessful);
+        // System.out.println("money " + petesCard.balance());
 
-        wasSuccessful = petesCard.takeMoney(4);
-        System.out.println("successfully withdrew: " + wasSuccessful);
-        System.out.println("money " + petesCard.balance());
+        // wasSuccessful = petesCard.takeMoney(4);
+        // System.out.println("successfully withdrew: " + wasSuccessful);
+        // System.out.println("money " + petesCard.balance());
+        PaymentTerminal unicafeExactum = new PaymentTerminal();
+
+        double change = unicafeExactum.eatAffordably(10);
+        System.out.println("remaining change " + change);
+
+        change = unicafeExactum.eatAffordably(5);
+        System.out.println("remaining change " + change);
+
+        change = unicafeExactum.eatHeartily(4.3);
+        System.out.println("remaining change " + change);
+
+        System.out.println(unicafeExactum);
  
     }
 }
