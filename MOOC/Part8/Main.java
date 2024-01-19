51051897718128a90d2600abe2d87d67aee68e39
Alexander Galev <alex.galev@yahoo.com>
2024-01-18 17:14:31 -0800

    part8

--- MOOC/Part8/Main.java
+++ MOOC/Part8/Main.java
@@ -43,7 +43,7 @@ public class Main {
         //     System.out.print(" ");
         // }
 
-        // System.out.println();
+        System.out.println();
 
         // HashMap<String, String> hashmap = new HashMap<>();
         // hashmap.put("f.e", "for example");
